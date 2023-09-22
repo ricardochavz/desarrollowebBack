c2aeb5a1415f7e8dc765e8426269c358bd1e176f
ricardochavz <ricardo.taracena64@gmail.com>
2023-09-22 16:17:51 -0600

    second commit

--- node/database/db.js
+++ node/database/db.js
@@ -1,7 +1,7 @@
 import {Sequelize} from 'sequelize'
 
-const db = new Sequelize('new_schema','root','1597',{
-host:'localhost',
+const db = new Sequelize('new_schema','admin','Desarrollo',{
+host:'database-2.co40ibmxerp8.us-east-2.rds.amazonaws.com',
 dialect:'mysql'
 })
 
